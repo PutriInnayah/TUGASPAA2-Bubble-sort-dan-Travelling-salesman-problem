a00f73be3e0a475a70de2601f1477119c9c97fb7
Putri Innayah <113874779+PutriInnayah@users.noreply.github.com>
2023-05-16 16:04:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+# Mendefinisikan fungsi bubble sort
 def bubbleSort(array):
+    
+    # Perulangan untuk menghitung panjang array
     for i in range(len(array)):
+        
+        # Swapping
         swapped = False
+        
+        # Perulangan untuk membandingkan elemen array
         for j in range(0, len(array) - i - 1):
 
+            # Proses perbandingan dan swapping
             if array[j] > array[j + 1]:
                 temp = array[j]
                 array[j] = array[j + 1]
@@ -10,13 +18,17 @@ def bubbleSort(array):
 
                 swapped = True
         print("Iterasi Ke-", i)
-
+ 
+        # Jika array sudah terurut maka swapping tidak di lakukan
         if not swapped:
             break
+# Pendeklarasian data yang akan di proses
 angka = [-10, 56, 34, 0, -5]
 print("Data : ", angka)
 
+# Pemanggilan fungsi bubble sort
 bubbleSort(angka)
 
+# Menampilkan hasil
 print('Hasil:')
 print(angka)
